40c65773f4e61282452189171343a2b02310ad26
Richard Hu <46357823+rzhanghu64@user.noreply.github.com>
2019-06-28 03:18:07 -0400

    update burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,20 +1,20 @@
-var orm = require("../config/orm.js");
+var orm = require("../config/orm");
 
 var burger = {
   all: function(callback) {
-    orm.selectAll("burger_db.burgers", function(res) {
+    orm.selectAll("burgers", function(res) {
       callback(res);
     });
   },
 
   create: function(cols, vals, callback) {
-    orm.insertOne("burger_db.burgers", cols, vals, function(res) {
+    orm.insertOne("burgers", cols, vals, function(res) {
       callback(res);
     });
   },
 
   update: function(newVal, condition, callback) {
-    orm.updateOne("burger_db.burgers", newVal, condition, function(res) {
+    orm.updateOne("burgers", newVal, condition, function(res) {
       callback(res);
     });
   }
